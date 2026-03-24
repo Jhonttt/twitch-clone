860eecf9ea781f666770b23c2acdc7bb00514cb2
Juan Atahona <juanatahonadev@gmail.com>
2026-03-24 15:26:47 +0100

    comp: create tag component

---
+++ app/components/ui/Tag.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  defineProps<{
+    label: string
+  }>()
+</script>
+
+<template>
+  <span class="bg-tag-bg text-tag-text rounded-full px-1.5 py-0.5 text-xs shrink-0">
+    {{ label }}
+  </span>
+</template>
