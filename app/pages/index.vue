2a2af25c25ef5337991e53bfd587cc7c95b38646
Juan Atahona <juanatahonadev@gmail.com>
2026-03-24 12:00:23 +0100

    feat: create mock data and the stream cards

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,23 +1,5 @@
-<script setup lang="ts">
-  const channelStore = useChannelsStore()
-</script>
-
 <template>
-  <main>
-    <section
-      :class="[
-        'grid gap-x-2 gap-y-5 mr-4',
-        channelStore.isOpen
-          ? 'grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'
-          : 'grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6',
-      ]"
-    >
-      <StreamCard />
-      <StreamCard />
-      <StreamCard />
-      <StreamCard />
-      <StreamCard />
-      <StreamCard />
-    </section>
+  <main class="mt-4">
+    <StreamCards />
   </main>
 </template>
